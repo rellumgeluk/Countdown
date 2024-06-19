2dbc787f5da0849aeb0de339f3975f8656478b3a
Nathalia <nathalia96234@gmail.com>
2024-06-19 18:02:46 +0200

    10-0

---
+++ script.js
@@ -0,0 +1,21 @@
+const output = document.querySelector('.output');
+
+let i=10;
+
+function Countdown(){
+    const para= document.createElement('p');
+    if (i === 10) {
+        para.textContent = "Countdown 10";
+    } else if (i === 0) {
+        para.textContent ="Blast off!";
+    } else {
+        para.textContent = i;
+    }
+    output.appendChild(para);
+    i--;
+
+    if (i<0) {
+        clearInterval(intervalId);
+    }
+}
+const intervalId = setInterval(Countdown, 1000); //1000 milliseconds (1 second)
